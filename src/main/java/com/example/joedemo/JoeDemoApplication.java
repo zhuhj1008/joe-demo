41518d646f78bae6a20f70b090983dfbbee4a708
zhuhj <1916931393@qq.com>
2022-01-04 16:47:32 +0800

    init

---
+++ src/main/java/com/example/joedemo/JoeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.joedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoeDemoApplication.class, args);
+    }
+
+}
